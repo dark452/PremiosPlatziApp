2e48b6256396783ef5bf0582f48feb630f192885
dark452 <imunoz@redeshost.cl>
2022-10-25 16:13:26 -0300

    Add polls view and url

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -4,4 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse("Estas en la pagina principal")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando la pregunta numero {question_id}")
