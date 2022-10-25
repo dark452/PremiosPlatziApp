b88114a4b49c108c7b786b895a34320ab12e06f1
dark452 <imunoz@redeshost.cl>
2022-10-25 20:45:54 -0300

    Create results template and fix results path name on urls.py

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
    ## example: /polls/5
    path("<int:question_id>/", views.detail, name="detail"),
    ## example: /polls/5/results
-   path("<int:question_id>/results/", views.results, name="result"),
+   path("<int:question_id>/results/", views.results, name="results"),
    ## example: /polls/5/vote
    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
