f829757066638d812a9026c3f6144fcaf531decb
dark452 <imunoz@redeshost.cl>
2022-10-25 16:48:49 -0300

    Add index template

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Estas en la pagina principal")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", 
+    {"latest_question_list": latest_question_list})
 
 
 def detail(request, question_id):
