cd1aca77f8f1c6de14c1c6a03e3cad9f0fbe7fbd
dark452 <imunoz@redeshost.cl>
2022-10-25 18:35:02 -0300

    Add poll form with choices

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
 from . import views
-
+app_name = "polls"
 urlpatterns = [
    ## example: /polls/
    path("", views.index, name="index"),
    ## example: /polls/5
-   path("<int:question_id>/", views.detail, name="index"),
+   path("<int:question_id>/", views.detail, name="detail"),
    ## example: /polls/5/results
-   path("<int:question_id>/results/", views.results, name="index"),
+   path("<int:question_id>/results/", views.results, name="result"),
    ## example: /polls/5/vote
-   path("<int:question_id>/vote/", views.vote, name="index"),
+   path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
