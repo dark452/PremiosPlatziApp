a4a8211ff5111b587a2a857a5c490c2edb48fab0
dark452 <imunoz@redeshost.cl>
2022-10-26 01:26:40 -0300

    Add test case for latest published questions

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+import datetime
 
+from time import timezone
+from django.test import TestCase
+from django.utils import timezone
+
+from . models import Question
 # Create your tests here.
+# Models and views
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_questions(self):
+        """was_published_recently returns False for questions whose pub_date is in the future"""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(question_text="Quies es el mejor Course Director de Platzi?", pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
